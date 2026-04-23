5d40486d8a1108c8a2e3270b89d72689a6b48d90
Lucy Burke <273163502+lucy-burke@users.noreply.github.com>
2026-04-23 11:19:49 -0500

    Add toolbar

--- app/src/main/java/com/seproject/plantry/PantryGroupFragment.java
+++ app/src/main/java/com/seproject/plantry/PantryGroupFragment.java
@@ -8,8 +8,12 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.seproject.plantry.adapter.PantryItemAdapter;
 import com.seproject.plantry.database.PantryDatabase;
 import com.seproject.plantry.database.PantryItem;
@@ -35,6 +39,9 @@ public class PantryGroupFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_group_pantry, container, false);
+        MaterialToolbar toolbar = root.findViewById(R.id.pantry_group_toolbar);
+        NavController navController = NavHostFragment.findNavController(this);
+        NavigationUI.setupWithNavController(toolbar, navController);
 
         return root;
     }
